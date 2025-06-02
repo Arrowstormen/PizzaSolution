de5ba6ee1ec9d835f4eb87a36890f5fad538c6f9
Mikkel <arrowstorm.videos@gmail.com>
2025-06-02 11:57:28 +0200

    Hardcoded GetMenu (check if lunch time works)

--- PizzaPlace/Services/MenuService.cs
+++ PizzaPlace/Services/MenuService.cs
@@ -4,7 +4,62 @@ public class MenuService : IMenuService
 {
     public Menu GetMenu(DateTimeOffset menuDate)
     {
-        throw new NotImplementedException("No menu has been implemented yet.");
+        var start = new TimeSpan(11, 0, 0);
+        var end = new TimeSpan(14, 0, 0);
+        if (TimeBetween(menuDate, start, end))
+        {
+            var lunchMenuItems = new ComparableList<MenuItem>
+        {
+             new MenuItem ("The Lunch Classic", Models.Types.PizzaRecipeType.StandardPizza, 90.00 ),
+             new MenuItem ("The Lunch Vegan", Models.Types.PizzaRecipeType.StandardPizza, 80.00 ),
+             new MenuItem ("The Freaky Lunch", Models.Types.PizzaRecipeType.OddPizza, 110.00 ),
+             new MenuItem ("The Horse Radish Lunch", Models.Types.PizzaRecipeType.HorseRadishPizza, 100.00 ),
+             new MenuItem ("The Tasty Extreme", Models.Types.PizzaRecipeType.ExtremelyTastyPizza, 140.00 ),
+             new MenuItem ("The Tasty Vegan Extreme", Models.Types.PizzaRecipeType.ExtremelyTastyPizza, 130.00 ),
+             new MenuItem ("The Desert", Models.Types.PizzaRecipeType.EmptyPizza, 55.00 ),
+             new MenuItem ("The Lunch Special", Models.Types.PizzaRecipeType.RarePizza, 100.00 ),
+             new MenuItem ("The Vegan Lunch", Models.Types.PizzaRecipeType.RarePizza, 95.00 ),
+             new MenuItem ("The Munch Lunch", Models.Types.PizzaRecipeType.OddPizza, 120.00 ),
+             new MenuItem ("The Kiddy Pool", Models.Types.PizzaRecipeType.StandardPizza, 75.00 ),
+             new MenuItem ("The Vegan Kiddy Pool", Models.Types.PizzaRecipeType.StandardPizza, 70.00 ),
+        };
+            var lunchMenu = new Menu("Standard Menu", lunchMenuItems);
+            return lunchMenu;
+        }
+        else
+        {
+            var standardMenuItems = new ComparableList<MenuItem>()
+        {
+             new MenuItem ("The Classic", Models.Types.PizzaRecipeType.StandardPizza, 100.00 ),
+             new MenuItem ("The Vegan", Models.Types.PizzaRecipeType.StandardPizza, 90.00 ),
+             new MenuItem ("The Freaky", Models.Types.PizzaRecipeType.OddPizza, 120.00 ),
+             new MenuItem ("The Horse Radish", Models.Types.PizzaRecipeType.HorseRadishPizza, 105.00 ),
+             new MenuItem ("The Tasty Extreme", Models.Types.PizzaRecipeType.ExtremelyTastyPizza, 150.00 ),
+             new MenuItem ("The Tasty Vegan Extreme", Models.Types.PizzaRecipeType.ExtremelyTastyPizza, 140.00 ),
+             new MenuItem ("The Desolation", Models.Types.PizzaRecipeType.EmptyPizza, 60.00 ),
+             new MenuItem ("The Dinner Special", Models.Types.PizzaRecipeType.RarePizza, 110.00 ),
+             new MenuItem ("The Vegan Dinner", Models.Types.PizzaRecipeType.RarePizza, 105.00 ),
+             new MenuItem ("The Chicken Dinner", Models.Types.PizzaRecipeType.OddPizza, 110.00 ),
+             new MenuItem ("The Kiddy Pool", Models.Types.PizzaRecipeType.StandardPizza, 80.00 ),
+             new MenuItem ("The Vegan Kiddy Pool", Models.Types.PizzaRecipeType.StandardPizza, 75.00 ),
+
+        };
+            var standardMenu = new Menu("Standard Menu", standardMenuItems);
+            return standardMenu;
+        }
+    }
+
+
+    //https://stackoverflow.com/questions/12998739/how-to-check-if-datetime-now-is-between-two-given-datetimes-for-time-part-only
+    private bool TimeBetween(DateTimeOffset datetime, TimeSpan start, TimeSpan end)
+    {
+        // convert datetime to a TimeSpan
+        TimeSpan now = datetime.TimeOfDay;
+        // see if start comes before end
+        if (start < end)
+            return start <= now && now <= end;
+        // start is after end, so do the inverse comparison
+        return !(end < now && now < start);
     }
 }
  
